b4371c3dcc077a71694157e1d64cd972c4959521
etozhestep <pans197@gmail.com>
2021-08-10 16:19:03 +0300

    initial commit

---
+++ tasks.py
@@ -0,0 +1,57 @@
+def get_coins(m0):
+    """
+    function, which returns the minimum number of
+    coins needed to pay a given amount of money
+    """
+    if not isinstance(m0, int):
+        raise TypeError
+    coins = {}
+
+    q1 = int(m0 / 100)
+    m1 = m0 - q1 * 100
+
+    q2 = int(m1 / 50)
+    m2 = m1 - q2 * 50
+
+    q3 = int(m2 / 20)
+    m3 = m2 - q3 * 20
+
+    q4 = int(m3 / 10)
+    m4 = m3 - q4 * 10
+
+    q5 = int(m4 / 5)
+    m5 = m4 - q5 * 5
+
+    q6 = int(m5 / 1)
+
+    coins[100] = q1
+    coins[50] = q2
+    coins[20] = q3
+    coins[10] = q4
+    coins[5] = q5
+    coins[1] = q6
+
+    return coins
+
+
+# print(get_coins(70.1))
+
+
+def check_palindrome(s):
+    """
+    Find whether the string is a palindrome
+    """
+    if not isinstance(s, str):
+        raise TypeError
+    s= s.lower()
+    size = len(s)
+    iter = size//2
+    #we need to iterate  only half of the string.
+    # Pull out the middle char if we have odd number of chars
+    for i in range(0, iter ):
+        if s[i] != s[size - i-1]:
+            return False
+    return True
+
+
+#print(check_palindrome(1))
---
+++ unitest.py
@@ -0,0 +1,44 @@
+import unittest
+from tasks import get_coins , check_palindrome
+
+
+class TestTask2(unittest.TestCase):
+    def test_get_all_coins(self):
+        """
+        Test  all possible coins variants
+        """
+        result = get_coins(186)
+        self.assertEqual(result, {1: 1, 5: 1, 10: 1, 20: 1, 50: 1, 100: 1})
+
+    def test_zero_coins(self):
+        """
+        Test  zero coins
+        """
+        result = get_coins(0)
+        self.assertEqual(result, {1: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0})
+
+    def test_bad_type(self):
+        with self.assertRaises(TypeError):
+            result = get_coins(1.1)
+
+class TestTask1(unittest.TestCase):
+    def test_get_all_coins(self):
+        """
+        Test  palindrome
+        """
+        result = check_palindrome("Abba")
+        self.assertTrue(result)
+
+    def test_zero_coins(self):
+        """
+        Test  zero coins
+        """
+        result = get_coins(0)
+        self.assertEqual(result, {1: 0, 5: 0, 10: 0, 20: 0, 50: 0, 100: 0})
+
+    def test_bad_type(self):
+        with self.assertRaises(TypeError):
+            result = check_palindrome(1001)
+
+if __name__ == '__main__':
+    unittest.main()
